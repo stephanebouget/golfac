22207a1dd9b0c26cf9e322f10124ac6bca76b6dd
Stephane Bouget <stephane.bouget@orange.com>
2019-10-09 19:39:30 +0200

    Add splashscreen.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {
+  Platform
+} from '@ionic/angular';
+import {
+  SplashScreen
+} from '@ionic-native/splash-screen/ngx';
+import {
+  StatusBar
+} from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
@@ -21,8 +29,13 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       // this.statusBar.styleDefault();
-      this.statusBar.styleBlackTranslucent();
-      this.splashScreen.hide();
+      // this.statusBar.styleBlackTranslucent();
+      // this.splashScreen.hide();
+      // let status bar overlay webview
+      // this.statusBar.overlaysWebView(true);
+
+      // set status bar to white
+      this.statusBar.backgroundColorByHexString('#464858');
     });
   }
 }
