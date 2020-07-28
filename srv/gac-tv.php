717b6cd86fcb72bf1d42db4054462a4d06f2d2ec
Stephane Bouget <stephane.bouget@orange.com>
2020-07-28 16:04:23 +0200

    Update TV prog.

--- srv/gac-tv.php
+++ srv/gac-tv.php
@@ -8,7 +8,7 @@ $content = file_get_contents('https://www.programme-tv.net/programme/chaine/prog
 $content = str_replace('</head>','
 <link rel="stylesheet" href="https://www.programme-tv.net/telfront/desktop/assets/styles/0.19194f4c31a23f572aa9.css" />
 <link rel="stylesheet" href="https://www.programme-tv.net/telfront/desktop/assets/styles/rwd-epg-channel.19194f4c31a23f572aa9.css" />
-<link rel="stylesheet" href="https://delphinecoiffure.000webhostapp.com/gac-tv.css" />
+<link rel="stylesheet" href="https://gac-srv.herokuapp.com/gac-tv.css" />
 
 </head>', $content);
 echo $content;
